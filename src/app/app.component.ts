7b8a64392252791a40739588e1a2f70dac716850
Nathan Daniels <nathandaniels@Nathans-MBP.attlocal.net>
2021-12-16 11:52:36 -0600

    Installs and did test plotly graph

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-api-project';
+
+  graph2 = {
+    data: [
+      { x: ['1979-12-12 12', '1979-12-12 13', '1979-12-12 14', '1979-12-12 15', '1979-12-12 16'], y: [1, 4, 9, 4, 1], type: 'scatter' },
+      { x: ['1979-12-12 12', '1979-12-12 13', '1979-12-12 14', '1979-12-12 15', '1979-12-12 16'], y: [1, 3, 6, 9, 6], type: 'scatter' },
+      { x: ['1979-12-12 12', '1979-12-12 13', '1979-12-12 14', '1979-12-12 15', '1979-12-12 16'], y: [1, 2, 4, 5, 6], type: 'scatter' },
+    ],
+    layout: {title: 'Some Data to Highlight'}
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
+import * as PlotlyJS from 'plotly.js-dist-min';
+import { PlotlyModule } from 'angular-plotly.js';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+PlotlyModule.plotlyjs = PlotlyJS;
 
 @NgModule({
   declarations: [
@@ -10,7 +17,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    HttpClientModule, 
+    PlotlyModule
   ],
   providers: [],
   bootstrap: [AppComponent]
